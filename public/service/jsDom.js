27b81035539cebe0a55a46ff84c319de094a0f36
Phillip-Mark <phillip.mark.england@gmail.com>
2022-05-16 16:57:52 -0500

    starting work on main functionality button

--- public/service/jsDom.js
+++ public/service/jsDom.js
@@ -1,29 +1,36 @@
 const jsDom = {
 
-    /*
-        CURRENT OPTIONS
-        element: HTML element,
-        className: String,
-        test: boolean
-    */
-    //checks an element to see if any direct children have a given class name
-    containsClass: (options) => {
-        let containsClass = false
-        for (x = 0; x < options.element.children.length; x++){
-            //testing all children
-            if (options.test == true){
-                console.log(options.element.children[x], options.element.children[x].className)
-            }
-            if (options.element.children[x].className.includes(options.className)){
-                containsClass = true
-                break
-            }
+    //will shrink all inputs with the given classname
+    shrinkInputsInCell: (className, buffer) => {
+        let elements = document.getElementsByClassName(className)
+        for (x = 0; x < elements.length; x++){
+            elements[x].style.width = elements[x].value.length + buffer + 'ch'
         }
-        //testing final result
-        if (options.test == true){
-            console.log(`Final Result: ${containsClass}`)
-        }
-        return containsClass
+    },
+
+    resizeColumnToLargest: (buffer, classesOfElements) => {
+        let largestWidth
+        //looping through all each of our passed in class names
+        Object.values(classesOfElements).forEach(classOfElements => {
+            largestWidth = 0
+            //looping through to find the largest width
+            for (x = 0; x < classOfElements.length; x++){
+                if (classOfElements[x].offsetWidth > largestWidth){
+                    largestWidth = classOfElements[x].offsetWidth
+                }
+            }
+            //looping through to set all elements width to the largest width
+            for (x = 0; x < classOfElements.length; x++){
+                //if we get an input, we have to set the size, not width
+                if (classOfElements[x].tagName == 'INPUT'){
+                    classOfElements[x].style.width = String(Number(largestWidth+buffer)) + 'px'
+                    continue
+                }
+                classOfElements[x].style.width = String(largestWidth+'px')
+            }
+        }) 
     }
+
+    
 
 }
